73cdafef9dfe494301fee533408fd0ce50907433
coder8086 <magare9727@gmail.com>
2025-07-15 21:11:03 +0530

    existing token problem

--- src/app/Pages/landing-page/landing-page.component.ts
+++ src/app/Pages/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component,CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { LandingNavComponent } from '../../Components/navbar/landing-nav/landing-nav.component';
 import { LandingFooterComponent } from '../../Components/footer/landing-footer/landing-footer.component';
 
@@ -10,6 +10,15 @@ import { LandingFooterComponent } from '../../Components/footer/landing-footer/l
   styleUrl: './landing-page.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+
+  ngOnInit(): void {
+      if (localStorage.getItem('token')){
+
+        localStorage.removeItem('token');
+        console.log("token exist");
+
+      }
+  }
 
 }
